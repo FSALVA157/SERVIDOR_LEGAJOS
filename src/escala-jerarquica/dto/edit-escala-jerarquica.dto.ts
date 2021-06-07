04d3aaaf626c32c2546e4e7a09fa5f7caa080b93
xxavierargentino <xxavierargentino@hotmail.com>
2021-06-07 10:04:22 -0300

    crud de escala jerarquica

--- src/escala-jerarquica/dto/edit-escala-jerarquica.dto.ts
+++ src/escala-jerarquica/dto/edit-escala-jerarquica.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateEscalaJerarquicaDto } from './create-escala-jerarquica.dto';
 
-export class EditEscalaJerarquica extends PartialType(CreateEscalaJerarquicaDto){}
+export class EditEscalaJerarquicaDto extends PartialType(CreateEscalaJerarquicaDto){}
