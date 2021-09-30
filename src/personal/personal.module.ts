16698cbe087f656af971f7b3a719fafcd2e9d082
Fernando Salva <xxavierargentino@hotmail.com>
2021-09-30 20:51:51 -0300

    agregado cloudinary a modulo de personal

--- src/personal/personal.module.ts
+++ src/personal/personal.module.ts
@@ -3,13 +3,14 @@ import { Personal } from "./entities/personal.entity";
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PersonalController } from './personal.controller';
 import { PersonalService } from './personal.service';
-
+import { CloudinaryModule } from '../cloudinary/cloudinary.module';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([
             Personal
-        ])
+        ]),
+        CloudinaryModule
     ],
     controllers: [PersonalController],
     providers: [PersonalService],
